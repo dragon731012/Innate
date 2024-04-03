36b6ea0eb10a439cdbd9e632b1f99e56a452cd98
Potato <105754685+dragon731012@users.noreply.github.com>
2024-04-03 09:29:10 -0500

    Update swears.js

--- swears.js
+++ swears.js
@@ -1,3 +1,12 @@
 function filter(text){
-    var temptext=text.replace("0","o");
+    var temptext=text.replace("0","o").replace("@","o").replace("1","i");
+    temptext=temptext.toLowerCase();
+    temptext.repalce(" ","");
+    for (var i=0;i<swears.length();i++){
+        var replacewith="";
+        for (var y=0;y<swears[i].length();y++){
+            replacewith=replacewith+"*";
+        }
+        temptext=temptext.replace(swears[i],replacewith);
+    }
 }
