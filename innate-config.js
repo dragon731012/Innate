92ee1e2bdef7cfc00b3a6cc52fe6d3851c65e70b
Potato <105754685+dragon731012@users.noreply.github.com>
2024-04-07 11:23:43 -0500

    Update innate-config.js

--- innate-config.js
+++ innate-config.js
@@ -1,24 +1,16 @@
-var innate={
-  server:"https://server.marcosrubn.com"
-};
-
-var backups=["https://innate-server.vercel.app/innate/https://server.marcosruben.com"];
-async function updateServer(){
-  try{
-    innate.server=new URL("/",innate.server);
-  } catch(e){
-    console.log("on backup");
-    for (var i=0;i<backups.length;i++){
-      var found=true;
-      try {
-        innate.server=new URL("/",backups[i]);
-      } catch (e) {
-        found=false;
-      }
-      if (found){
-        console.log("backup found, using server "+innate.server);
-        break;
-      }
+var servers=["https://server.marcosrubn.com","https://innate-server.vercel.app/innate/https://server.marcosruben.com"];
+async function getServer(){
+  var server="";
+  for (var i=0;i<servers.length;i++){
+    var found=true;
+    try {
+      server=new URL("/",servers[i]);
+    } catch (e) {
+      found=false;
+    }
+    if (found){
+      console.log("server found, using server "+server);
+      return server;
     }
   }
 }
