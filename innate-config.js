ceef7822ea6ba0f0d3f4d112803123e1f624c394
Potato <105754685+dragon731012@users.noreply.github.com>
2024-04-07 11:55:33 -0500

    Update innate-config.js

--- innate-config.js
+++ innate-config.js
@@ -1,26 +1,24 @@
-/*function credit to chatgpt*/
-function isUrlReachable(url) {
-  return new Promise(resolve => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('HEAD', url);
-    xhr.onload = () => resolve(xhr.status >= 200 && xhr.status < 300);
-    xhr.onerror = () => resolve(false);
-    xhr.send();
-  });
-}
+function valid(url){
+    return new Promise((resolve) => {
+      fetch(url)
+        .then(response => {
+          resolve(true);
+        })
+        .catch(error => {
+          resolve(false);
+        });
+    });
+  }
 
 const servers=["https://server.marcosruben.com","https://innate-server.vercel.app/innate/https://server.marcosruben.com"];
 async function getServer(){
   var server="";
   for (var i=0;i<servers.length;i++){
-    isUrlReachable(servers[i])
-      .then(reachable => {
-        if (reachable) {
-          console.log("server found, using server "+servers[i]);
-          return new URL("/",servers[i]);;
-        } else {
-          console.log('server '+servers[i]+" failed, alternating...");
-        }
-    });
+    if (valid(servers[i]){
+      console.log("server found, using server "+servers[i]);
+      return new URL("/",servers[i]);;
+    } else {
+      console.log('server '+servers[i]+" failed, alternating...");
+    }
   }
 }
