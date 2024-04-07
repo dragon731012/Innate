c69cac39bcaec14df1dbc29993c12fb8a23e1b28
Potato <105754685+dragon731012@users.noreply.github.com>
2024-04-07 13:19:19 -0500

    Update innate-config.js

--- innate-config.js
+++ innate-config.js
@@ -1,28 +1,28 @@
 const servers=["https://server.marcosruben.com","https://innate-server.vercel.app/innate/https://server.marcosruben.com"];
 
-async function findConnectableURL() {
-    for (let url of servers) {
-        try {
-            await fetch(url);
-            return url;
-        } catch (error) {
-            console.error(`Failed to connect to ${url}:`, error);
+function getText(file){
+    return new Promise((resolve) => {
+      fetch(file)
+        .then((response) => {
+          return response.text();
+        })
+        .then((txt) => {
+          resolve(txt.trim());
+        });
+    });
+  }
+
+async function getServer(){
+    for (var i=0;i<servers.length;i++){
+        var found=true;
+        try{
+            getText(servers[i]);
+        } catch (e) {
+            found=false;
+        }
+        if (found){
+            return new URL("/",servers[0]);
         }
     }
     return null;
-}
-
-async function getServer(){
-    return new URL("/",servers[0]);
-    /*
-  return findConnectableURL()
-    .then(connectableURL => {
-        if (connectableURL) {
-            return connectableURL;
-        } else {
-            console.log('No connectable URL found.');
-        }
-    })
-    .catch(error => console.error('Error finding connectable URL:', error));
-    */
 }
