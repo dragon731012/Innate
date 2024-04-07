911a4235bac17c20ea9e5f1fd67367fa561e46cf
Potato <105754685+dragon731012@users.noreply.github.com>
2024-04-07 11:30:46 -0500

    Update innate-config.js

--- innate-config.js
+++ innate-config.js
@@ -1,16 +1,20 @@
+/*function credit to https://www.freecodecamp.org/news/check-if-a-javascript-string-is-a-url*/
+const isValidUrl = urlString=> {
+      try { 
+      	return Boolean(new URL(urlString)); 
+      }
+      catch(e){ 
+      	return false; 
+      }
+  }
+
 var servers=["https://server.marcosrubn.com","https://innate-server.vercel.app/innate/https://server.marcosruben.com"];
 async function getServer(){
   var server="";
   for (var i=0;i<servers.length;i++){
-    var found=true;
-    try {
-      server=new URL("/",servers[i]);
-    } catch (e) {
-      found=false;
-    }
-    if (found){
-      console.log("server found, using server "+server);
-      return server;
+    if (isValidUrl(servers[i])){
+      console.log("server found, using server "+servers[i]);
+      return servers[i];
     }
   }
 }
