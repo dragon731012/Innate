f4287cc2fac3d214977cb07960be397d90326f80
Potato <105754685+dragon731012@users.noreply.github.com>
2024-04-07 11:12:10 -0500

    Update innate-config.js

--- innate-config.js
+++ innate-config.js
@@ -2,4 +2,21 @@ const innate={
   server:"https://server.marcosruben.com"
 };
 
-innate.server=new URL("/",innate.server);
+var backups=["https://innate-server.vercel.app/innate/https://server.marcosruben.com"];
+async function updateServer(){
+  try{
+    innate.server=new URL("/",innate.server);
+  } catch(e){
+    for (var i=0;i<backups.length;i++){
+      var found=true;
+      try {
+        innate.server=new URL("/",backups[i]);
+      } catch (e) {
+        found=false;
+      }
+      if (found){
+        break;
+      }
+    }
+  }
+}
