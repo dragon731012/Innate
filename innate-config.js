1164c40a120cb287067ae08c409630b02d4f6416
Potato <105754685+dragon731012@users.noreply.github.com>
2024-04-07 11:37:51 -0500

    Update innate-config.js

--- innate-config.js
+++ innate-config.js
@@ -1,20 +1,26 @@
-/*function credit to https://www.freecodecamp.org/news/check-if-a-javascript-string-is-a-url*/
-const isValidUrl = urlString=> {
-      try { 
-      	return Boolean(new URL(urlString)); 
-      }
-      catch(e){ 
-      	return false; 
-      }
+/*function credit to chatgpt*/
+async function isUrlReachable(url) {
+  try {
+    const response = await fetch(url, { method: 'HEAD' });
+    return response.ok;
+  } catch (error) {
+    console.error('Error checking URL reachability:', error);
+    return false;
   }
+}
 
 var servers=["https://server.marcosrubn.com","https://innate-server.vercel.app/innate/https://server.marcosruben.com"];
 async function getServer(){
   var server="";
   for (var i=0;i<servers.length;i++){
-    if (isValidUrl(servers[i])){
-      console.log("server found, using server "+servers[i]);
-      return servers[i];
-    }
+    isUrlReachable(servers[i])
+      .then(reachable => {
+        if (reachable) {
+          console.log("server found, using server "+servers[i]);
+          return servers[i];
+        } else {
+          console.log('server '+servers[i]+" failed, alternating...");
+        }
+    });
   }
 }
