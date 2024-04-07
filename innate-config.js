d8ebace06cefb455329fb47485394e1aed5c7e30
Potato <105754685+dragon731012@users.noreply.github.com>
2024-04-07 10:52:04 -0500

    Update innate-config.js

--- innate-config.js
+++ innate-config.js
@@ -1,5 +1,17 @@
 const innate={
-  server:"https://server.marcosruben.com"
+  server1:"https://server.marcosrub.com",
+  server2:"https://innate-server.vercel.app"
 };
 
-innate.server=new URL("/",innate.server);
+var oldi=-1;
+for (var i=0;i<innate.length){
+  if (oldi=i){
+    break;
+  }
+  try{
+    innate.server=new URL("/",innate.server1);
+  } catch {
+    i++;
+  }
+  oldi++;
+}
