dbc7c82dd5bbde1159e65b447954aba2e32d88a5
Potato <105754685+dragon731012@users.noreply.github.com>
2024-04-09 16:10:55 -0500

    Update innate-config.js

--- innate-config.js
+++ innate-config.js
@@ -2,4 +2,19 @@ var innate={
   server:"https://server.marcosruben.com"
 };
 
+async function checkWebsite(url) {
+    try {
+        const response = await fetch(url);
+        
+        if (response.ok) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        return false;
+    }
+}
+
+
 innate.server=new URL("/",innate.server);
