8223a70f75a0fb9ea607ff4b065abb3a01dd70cb
Potato. <105754685+dragon731012@users.noreply.github.com>
2024-09-05 20:20:34 -0500

    Delete register-sw.js

--- register-sw.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then((registration) => {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }).catch((error) => {
-            console.log('ServiceWorker registration failed: ', error);
-        });
-    });
-}
