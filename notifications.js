f727e142cbea9c58a51fc254e7741398e5a22878
Potato. <105754685+dragon731012@users.noreply.github.com>
2024-09-05 20:34:40 -0500

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -1,17 +1,22 @@
-if (Notification.permission!="granted"){
+if (Notification.permission !== "granted") {
   Notification.requestPermission().then((permission) => {
-      if (permission === "granted") {
-        
-      }
-    });
+    if (permission === "granted") {
+      createNotification();
+    }
+  });
+} else {
+  createNotification();  
 }
-const notification = new Notification("Test notification", {
-  body: "test notification"
-});
 
-notification.onclick = function (event) {
-  event.preventDefault();
-  window.open(window.location.href, '_blank');
-};
+function createNotification() {
+  const notification = new Notification("Test notification", {
+    body: "test notification"
+  });
 
-setTimeout(notification.close.bind(notification), 5000);
+  notification.onclick = function (event) {
+    event.preventDefault();
+    window.open(window.location.href, '_blank'); 
+  };
+
+  setTimeout(notification.close.bind(notification), 5000);
+}
