923a2b98ecd65d956f2e9fe9d481df069555b9d5
Potato. <105754685+dragon731012@users.noreply.github.com>
2024-11-10 14:41:12 -0600

    Update innate-config.js

--- innate-config.js
+++ innate-config.js
@@ -1,4 +1,27 @@
 var innate={
   server:"https://innate.ape3d.com/server/"
 };
+
+function canReach(url){
+  return new Promise((resolve) => {
+    fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    })
+    .catch(() => resolve(false));
+  });
+}
+
+async function checkIfReachable(){
+  if (!await canReach(innate.server+"index.php")){
+    document.body.innerHTML="Unable to reach server! Please contact the website owner to notify them of this problem. If you are the owner, please contact dragon73101@gmail.com for help resolving this issue.";
+  }
+}
+
 localStorage.setItem("server",innate.server);
+
+checkIfReachable();
